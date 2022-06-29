04691886844b1f1e838b0d47658ce781d940db93
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-06-29 15:46:10 -0400

    refactored coordinates pull

--- assets/scripts/mountains_info.js
+++ assets/scripts/mountains_info.js
@@ -1,39 +1,64 @@
+//Query document for necessary elements.
 let mountainsOutPutContainer = document.querySelector(".mountainsOutPutContainer");
 let mountainDataList = document.querySelector("#mountainDataList");
 let showAllMountainsButton = document.querySelector("#showAllMountainsButton");
-console.log(showAllMountainsButton)
 
+//Add event listener to the dropdown change event.
 mountainDataList.addEventListener("change", () => {
+
+    //Remove the no display class from the output container so elements can be loaded to it and visible.
     mountainsOutPutContainer.classList.remove("d-none");
+
+    //Reset the output container in case it already has data.
     mountainsOutPutContainer.innerHTML = "";
-    console.log(mountainDataList.value)
+    
+    //Look through the mountains array for a match to the dropdown's value.
     let matches = mountainsArray.filter((mountain) => mountain.name === mountainDataList.value)
-    console.log(matches)
+    
+    //Call the display mountains function with the matches array and output container as inputs.
     displayMoutains(matches, mountainsOutPutContainer)
 })
 
+//Add event listner to the show all mountains button on click event.
 showAllMountainsButton.addEventListener("click", () => {
+
+    //Show the montains output container.
     mountainsOutPutContainer.classList.remove("d-none")
+
+    //Call the display mountains function with the full mountains array and the output container as inputs.
     displayMoutains(mountainsArray, mountainsOutPutContainer);
 })
 
-
+//Function do load mountains data given an array and the container for output.
 function displayMoutains(matches, outputContainer) {
     
+    //Set how many cards per row you want.
     let cardsPerRow = 3;
+
+    //Get the rows to add by taking the length of the array and dividing by the cards per row. Round up so any remainder has a row to go in.
     let rowsToAdd = Math.ceil(matches.length / cardsPerRow)
 
+    //Add rows to the container.
     for(let i = 1; i <= rowsToAdd; i++) {
         outputContainer.innerHTML += `<div class="row p-3 mountainRows"></div>`
     }
     
-    let newRows = document.querySelectorAll(".mountainRows")
+    //Find the newly added rows.
+    let newRows = document.querySelectorAll(".mountainRows");
+
+    //Set the columns added to 0
     let columnsAdded = 0;
     let rowIndex = 0;
+
+    //Loop over the matches array and load each mountain.
     matches.forEach((mountain) => {
+
+        //If we hit a multiple of the cards per row value, up the index by one to move to the next row. Unless we're on the first one.
         if(columnsAdded % cardsPerRow === 0 && columnsAdded !== 0) {
             rowIndex++
         }
+
+        //Look at the new rows array and use the row index to get the current row. Add the column div data using the current mountain value.
         newRows[rowIndex].innerHTML += `
             <div class="col p-3">
                 <div class="card">
@@ -49,19 +74,27 @@ function displayMoutains(matches, outputContainer) {
                 </div>
             </div>`
 
+        //Increment the columns added value.
         columnsAdded++
 
-        getSunsetForMountain(mountain.coords.lat, mountain.coords.lng).then((data) => {
+        //Call the sunset info function to get the data for the mountains sunset/sunrise times. Only do it if we have one match... seeing timeouts
+        //with the API and not sure if this is because of the large amount of calls...
+        matches.length === 1 ? getSunsetForMountain(mountain.coords.lat, mountain.coords.lng).then((data) => {
 
+            //Find the current data values, use the mountainCard id's that were used in the column div.
             let sunriseData = document.querySelector(`#mountainCard${mountain.name.replace(" ", "").replace(".", "").replace("'", "")} > p.sunrise`);
-
             let sunsetData = document.querySelector(`#mountainCard${mountain.name.replace(" ", "").replace(".", "").replace("'", "")} > p.sunset`);
 
+            //Add the sunrise data results from the fetch call.
             sunriseData.innerHTML = `<b>Sunrise: </b>${data.results.sunrise}`
             sunsetData.innerHTML = `<b>Sunset: </b>${data.results.sunset}`
+
+            //Show the sunrise/sunset data.
             sunriseData.classList.remove("d-none")
             sunsetData.classList.remove("d-none")
-        })
+
+        }) : ""
+        
     })
 }
 
--- assets/scripts/parks_search.js
+++ assets/scripts/parks_search.js
@@ -25,13 +25,18 @@ let parkTypeListContainer = document.querySelector(".parkTypeListContainer");
 //Container for output to hide/reveal when radio is checked and a selection is made
 let parkTypeOutPutContainer = document.querySelector(".parkTypeOutPutContainer");
 
+//Add an event listener to the park type radio search option.
 parkTypeRadio.addEventListener("click", () => {
 
+    //If clicked, hide and remove data from the output containers.
     locationsListContainer.classList.add("d-none");
     locationOutPutContainer.classList.add("d-none")
     parkTypedatalistOptions.innerHTML = "";
+
+    //Show the park type dropdown.
     parkTypeListContainer.classList.remove("d-none");
 
+    //Loop over the park type array and add the options to the datalist.
     parkTypesArray.forEach((parkType) => {
 
         parkTypedatalistOptions.innerHTML += `<option value="${parkType}">${parkType}</option>`;
@@ -39,30 +44,54 @@ parkTypeRadio.addEventListener("click", () => {
     });
 });
 
+//Add an event listener to the location search radio.
 locationRadio.addEventListener("click", () => {
+
+    //If clicked, hide and remove data from the output containers.
     parkTypeListContainer.classList.add("d-none");
     parkTypeOutPutContainer.classList.add("d-none")
     parkTypedatalistOptions.innerHTML = "";
+
+    //Show the locations dropdown.
     locationsListContainer.classList.remove("d-none");
+
+    //Loop over each item in the location array and add it as an option to the datalist.
     locationsArray.forEach((location) => {
         parksSearchOptions.innerHTML += `<option value="${location}">${location}</option>`
     })
 })
 
+//When a change event occurs on the location search datalist...
 parksSearchDataList.addEventListener("change", () => {
+
+    //Show the output container.
     locationOutPutContainer.classList.remove("d-none")
+
+    //Look for matches of the selected value in the national parks array.
     let matches = nationalParksArray.filter((park) => park.State === parksSearchDataList.value)
+
+    //Call the display parks function.
     displayParks(matches, locationOutPutContainer)
+
 })
 
+//Add an event listener to the park type datalist.
 parkTypeDataList.addEventListener("change", () => {
+
+    //Show the park type output container.
     parkTypeOutPutContainer.classList.remove("d-none");
+
+    //Find matches in the national parks array for the selected value in the location name.
     let matches = nationalParksArray.filter((park) => park.LocationName.toUpperCase().includes(parkTypeDataList.value.toUpperCase()) === true)
-    console.log(matches)
+
+    //Call the display parks function
     displayParks(matches, parkTypeOutPutContainer)
 })
 
+//Function to display parks given an array of parks and a container to load them in.
 function displayParks(matches, outputContainer) {
+
+    //Set cards per row
     let cardsPerRow = 3;
     let rowsToAdd = Math.ceil(matches.length / cardsPerRow)
 
@@ -94,18 +123,6 @@ function displayParks(matches, outputContainer) {
             </div>`
 
         columnsAdded++
-
-        getSunsetForMountain(park.Latitude, park.Longitude).then((data) => {
-            console.log(data)
-            let sunriseData = document.querySelector(`#parkCard${park.LocationID} > p.sunrise`);
-
-            let sunsetData = document.querySelector(`#parkCard${park.LocationID} > p.sunset`);
-
-            sunriseData.innerHTML = `<b>Sunrise: </b>${data.results.sunrise}`
-            sunsetData.innerHTML = `<b>Sunset: </b>${data.results.sunset}`
-            sunriseData.classList.remove("d-none")
-            sunsetData.classList.remove("d-none")
-        })
     })
 }
 
@@ -117,10 +134,21 @@ showAllParksButton.addEventListener("click", () => {
     displayParks(nationalParksArray, locationOutPutContainer);
 })
 
-async function getSunsetForMountain(lat, lng){
+async function getSunsetForParks(lat, lng){
     let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
     let data = await response.json()
     return data
 }
 
+////The below can be added to the display parks function if numerous calls to the api are OK.
+// getSunsetForMountain(park.Latitude, park.Longitude).then((data) => {
 
+//     let sunriseData = document.querySelector(`#parkCard${park.LocationID} > p.sunrise`);
+
+//     let sunsetData = document.querySelector(`#parkCard${park.LocationID} > p.sunset`);
+
+//     sunriseData.innerHTML = `<b>Sunrise: </b>${data.results.sunrise}`
+//     sunsetData.innerHTML = `<b>Sunset: </b>${data.results.sunset}`
+//     sunriseData.classList.remove("d-none")
+//     sunsetData.classList.remove("d-none")
+// })
