9d7522e52b0f3750359629c3be63c16912c395e9
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-06-29 09:12:42 -0400

    completed search output layout

--- assets/scripts/parks_search.js
+++ assets/scripts/parks_search.js
@@ -1,27 +1,44 @@
+//Parks Search data list
 let parksSearchDataList = document.querySelector("#exampleDataList");
 let parksSearchOptions = document.querySelector("#datalistOptions");
+
+//Radio references
 let locationRadio = document.querySelector("#locationRadio");
 let parkTypeRadio = document.querySelector("#parkTypeRadio");
+
+//Container for drop down to hide/reveal when radio is checked
 let locationsListContainer = document.querySelector(".locationsListContainer")
+
+//Container for output to hide/reveal when radio is checked and a selection is made
 let locationOutPutContainer = document.querySelector(".locationOutPutContainer");
-let parkTypedatalistOptions = document.querySelector("#parkTypedatalistOptions");
+
+//Search by park type data list input and options
 let parkTypeDataList = document.querySelector("#parkTypeDataList")
+let parkTypedatalistOptions = document.querySelector("#parkTypedatalistOptions");
+
+//Container for drop down to hide/reveal when radio is checked
 let parkTypeListContainer = document.querySelector(".parkTypeListContainer");
+
+//Container for output to hide/reveal when radio is checked and a selection is made
 let parkTypeOutPutContainer = document.querySelector(".parkTypeOutPutContainer");
 
 parkTypeRadio.addEventListener("click", () => {
+
     locationsListContainer.classList.add("d-none");
+    locationOutPutContainer.classList.add("d-none")
     parkTypedatalistOptions.innerHTML = "";
     parkTypeListContainer.classList.remove("d-none");
-    console.log(parkTypesArray)
+
     parkTypesArray.forEach((parkType) => {
-        console.log(parkType)
-        parkTypedatalistOptions.innerHTML += `<option value="${parkType}">${parkType}</option>`
-    })
+
+        parkTypedatalistOptions.innerHTML += `<option value="${parkType}">${parkType}</option>`;
+
+    });
 });
 
 locationRadio.addEventListener("click", () => {
     parkTypeListContainer.classList.add("d-none");
+    parkTypeOutPutContainer.classList.add("d-none")
     parkTypedatalistOptions.innerHTML = "";
     locationsListContainer.classList.remove("d-none");
     locationsArray.forEach((location) => {
@@ -32,18 +49,43 @@ locationRadio.addEventListener("click", () => {
 parksSearchDataList.addEventListener("change", () => {
     locationOutPutContainer.classList.remove("d-none")
     let matches = nationalParksArray.filter((park) => park.State === parksSearchDataList.value)
-    console.log(matches)
-    matches.forEach((park) => {
-            
-            locationOutPutContainer.innerHTML += `<div class="row">${JSON.stringify(park)}<div>`
-    })
-    
+    displayParks(matches, locationOutPutContainer)
 })
-console.log(parkTypeDataList)
+
 parkTypeDataList.addEventListener("change", () => {
     parkTypeOutPutContainer.classList.remove("d-none");
     let matches = nationalParksArray.filter((park) => park.LocationName.includes(parkTypeDataList.value))
-    
     console.log(matches)
+    displayParks(matches, parkTypeOutPutContainer)
 })
+
+function displayParks(matches, outputContainer) {
+    let cardsPerRow = 3;
+    let rowsToAdd = Math.ceil(matches.length / cardsPerRow)
+
+    for(let i = 1; i <= rowsToAdd; i++) {
+        outputContainer.innerHTML += `<div class="row p-3 parkRows"></div>`
+    }
     
+    let newRows = document.querySelectorAll(".parkRows")
+
+    let columnsAdded = 0;
+    let rowIndex = 0;
+    matches.forEach((park) => {
+        if(columnsAdded % cardsPerRow === 0 && columnsAdded !== 0) {
+            rowIndex++
+        }
+        newRows[rowIndex].innerHTML += `
+            <div class="col p-3 parkCards">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${park.LocationName}</h5>
+                        <p class="card-text">${park.Address}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>`
+
+        columnsAdded++
+    })
+}
